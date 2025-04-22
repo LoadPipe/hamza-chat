5f8b1691057ce6393239350315af92d6aaeb870b
Tronky <john.kosinski@gmail.com>
2025-04-22 14:24:13 +0700

    client

---
+++ chat-client/etc/head.php
@@ -0,0 +1,43 @@
+<?php
+	header("Access-Control-Allow-Origin: *");
+?>
+<title>HNSChat</title>
+<meta charset="utf-8">
+<meta name="google" content="notranslate"> 
+<meta name="darkreader" content="noplz">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+<meta name="format-detection" content="telephone=no">
+
+<meta name="title" content="HNSChat">
+<meta name="description" content="HNSChat is a free end-to-end encrypted messaging platform where you chat using your Handshake names.">
+<meta property="og:type" content="website">
+<meta property="og:url" content="https://hns.chat/">
+<meta property="og:title" content="HNSChat">
+<meta property="og:description" content="HNSChat is a free end-to-end encrypted messaging platform where you chat using your Handshake names.">
+<meta property="og:image" content="https://hns.chat/assets/img/cover">
+<meta property="twitter:card" content="summary_large_image">
+<meta property="twitter:url" content="https://hns.chat/">
+<meta property="twitter:title" content="HNSChat">
+<meta property="twitter:description" content="HNSChat is a free end-to-end encrypted messaging platform where you chat using your Handshake names.">
+<meta property="twitter:image" content="https://hns.chat/assets/img/cover">
+<link rel="manifest" href="/manifest.json">
+<link href="https://fonts.googleapis.com/css2?family=Rubik&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="/assets/css/style?r=1">
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+<script type="text/javascript" src="https://auth.varo.domains/v1"></script>
+<script type="text/javascript" src="/assets/js/qr?r=1"></script>
+<script type="text/javascript" src="/assets/js/qr2?r=1"></script>
+<script type="text/javascript" src="/assets/js/he?r=1"></script>
+<script type="text/javascript" src="/assets/js/zwj?r=1"></script>
+<script type="text/javascript" src="/assets/js/date?r=1"></script>
+<script type="text/javascript" src="/assets/js/emojis?r=1"></script>
+<script type="text/javascript" src="/assets/js/anchorme?r=1"></script>
+<script type="text/javascript" src="/assets/js/mask?r=1"></script>
+<script type="text/javascript" src="/assets/js/confetti?r=1"></script>
+<script type="text/javascript" src="/assets/js/dish?r=1"></script>
+<script type="text/javascript" src="/assets/js/janus?r=1"></script>
+<script type="text/javascript" src="/assets/js/adapter?r=1"></script>
+<script type="module" src="/assets/js/script?r=1"></script>
+<script type="text/javascript">
+	var revision = 1; 
+</script>
---
+++ chat-client/index.php
@@ -0,0 +1,12 @@
+<?php
+	include "etc/includes.php";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<?php include "etc/head.php"; ?>
+</head>
+<?php
+	include "etc/page/chat.php";
+?>
+</html>
